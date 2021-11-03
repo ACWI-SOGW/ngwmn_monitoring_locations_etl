e4ea9c51c1a5160cab1b58a32b64b663c0249ad0
David Uselmann <duselmann@usgs.gov>
2021-11-03 16:16:06 -0500

    better error handling and logging
    the unit test currently has a race condition that I expected and will covert the extract script into a class to avoid global variables

--- etl/extract.py
+++ etl/extract.py
@@ -4,22 +4,146 @@ Functions to retrieve data from the Well Registry API
 
 import logging
 
+from time import sleep
+from json.decoder import JSONDecodeError
 from requests import Session
+from requests.exceptions import RequestException
+from requests.exceptions import HTTPError
+
+"""Number of records to fetch in at once"""
+FETCH_LIMIT = 8
+"""Number of times to retry when there is a network error"""
+FETCH_TRIES_FOR_NETWORK_ERROR = 2
+"""Number of times to retry when there is an HTTP Status code other than ok"""
+FETCH_TRIES_FOR_STATUS_CODE = 2
+"""Number of times to retry when there is the JSON fails to decode"""
+FETCH_TRIES_FOR_JSON = 2
+"""Number of times to accept a missing data for aborting the ETL"""
+FETCH_JSON_ERROR_TOLERANCE = 2
+"""Number of seconds to wait before a retry is attempted"""
+FETCH_RETRY_DELAY = 10
+"""Number of fetches per info log"""
+FETCHES_PER_LOG = 128
 
 
 def get_monitoring_locations(registry_ml_endpoint):
     """
     Get the monitoring location data.
     """
-    next_chunk = registry_ml_endpoint
+    # initialize state of errors, URL, and results
+    json_fail_count = 0
+    fetches = 0
+    next_chunk = construct_url(registry_ml_endpoint)
     results = []
+
     with Session() as session:
         while next_chunk:
-            logging.info(f'Retrieving monitoring locations: {next_chunk}')
-            resp = session.get(next_chunk)
-            payload = resp.json()
-            results.extend(payload['results'])
-            next_chunk = payload['next']
+            fetches += 1
+            if fetches % FETCHES_PER_LOG == 0:
+                logging.info(f'Retrieving monitoring locations: {next_chunk}')
+            try:
+                payload = fetch_record_block(next_chunk, session)
+                results.extend(payload.get('results'))
+                next_chunk = payload.get('next')
+            except JSONDecodeError as json_err:
+                json_fail_count += 1
+                if json_fail_count > FETCH_JSON_ERROR_TOLERANCE:
+                    logging.error('Abort: JSON errors exceeded. Set FETCH_JSON_ERROR_TOLERANCE to fine tune.')
+                    raise JSONDecodeError('JSON errors exceeded.', json_err.doc, json_err.pos)
+                else:
+                    logging.warning(f'JSON error occurred, {FETCH_JSON_ERROR_TOLERANCE-json_fail_count} before abort.')
+            except RequestException:
+                logging.error(f'Unrecoverable error fetching data from {next_chunk}')
+                return []
 
     logging.info(f'Finished retrieving {len(results)} monitoring locations.')
     return results
+
+
+def fetch_record_block(url, session):
+    attempt_count_net = 1
+    attempt_count_status = 1
+    attempt_count_json = 1
+    attempts_remain = True
+
+    while attempts_remain:
+        # if this is a retry then pause for a delay to see if it recovers
+        if attempt_count_net + attempt_count_status + attempt_count_json > 3:
+            logging.warning(f'Retrying request in {FETCH_RETRY_DELAY} seconds')
+            sleep(FETCH_RETRY_DELAY)
+
+        try:
+            payload = try_fetch(url, session)
+            attempts_remain = False  # indicate that we are done
+        except HTTPError as se:  # trap http status error before the more general RequestException
+            attempt_count_status += 1
+            logging.warning(f'HTTP status code, {se.response.status_code}, from URL: {url}')
+        except RequestException as re:  # trap network issues
+            attempt_count_net += 1
+            if re.response is None:
+                logging.warning(f'No response entity from URL: {url}')
+            else:
+                logging.warning(f'Exception requesting URL: {url}')
+        except JSONDecodeError as json_err:  # trap JSON parsing errors
+            recent_json_error = json_err
+            attempt_count_json += 1
+            logging.warning(f'JSON parsing error in response from: {url}')
+            logging.warning(json_err)
+            logging.warning(json_err.doc)
+
+        # exit loop if done/success or too many tries of any type
+        attempts_remain &= attempt_count_net <= FETCH_TRIES_FOR_NETWORK_ERROR \
+            and attempt_count_status <= FETCH_TRIES_FOR_STATUS_CODE \
+            and attempt_count_json <= FETCH_TRIES_FOR_JSON
+
+    # allow for the caller to know how often the JSON was bad
+    if attempt_count_json > FETCH_TRIES_FOR_JSON:
+        doc = recent_json_error.doc if recent_json_error is None else ''
+        pos = recent_json_error.pos if recent_json_error is None else 0
+        raise JSONDecodeError("Exceeded JSON Tries.", doc, pos)
+
+    if attempt_count_net > FETCH_TRIES_FOR_NETWORK_ERROR or attempt_count_status > FETCH_TRIES_FOR_STATUS_CODE:
+        logging.warning(f'Retrying failed')
+        raise RequestException()
+
+    # if this is a retry then pause for a delay to see if it recovers
+    if attempt_count_net + attempt_count_status + attempt_count_json > 3:
+        logging.info(f'Retrying succeeded')
+
+    return payload
+
+
+def try_fetch(url, session):
+    respsponse = session.get(url)
+
+    if respsponse is None:  # trap no response
+        raise RequestException()
+
+    # status codes above 203 are reduced content status codes - that is bad
+    if respsponse.status_code >= 204:  # trap bad status code
+        raise respsponse.raise_for_status()
+
+    try:
+        json = respsponse.json()
+    except JSONDecodeError as json_err:
+        # put the response text on the exception
+        json_err.doc = respsponse.text
+        raise json_err
+
+    return json
+
+
+def construct_url(endpoint):
+    """
+    Construct the URL with a smaller limit than the 1024 default
+    """
+    url = endpoint
+
+    if 'limit' not in url:
+        if '?' not in url:
+            url += '?'
+        else:
+            url += '&'
+        url += 'limit=' + str(FETCH_LIMIT)
+
+    return url
--- etl/test/test_extract.py
+++ etl/test/test_extract.py
@@ -1,11 +1,18 @@
 """
 Tests for the extract.py module
 """
+
+# unit testing modules
 from unittest import TestCase, mock
+import mockito, logging
+from mockito import when, mock as mocki
 
-from ..extract import get_monitoring_locations
+# modules referenced during testing
+from json import JSONDecodeError
+from requests import Response, Session, HTTPError, RequestException
 
-from requests import Session
+# file under test
+from .. import extract
 
 
 class TestGetMonitoringLocations(TestCase):
@@ -23,8 +30,206 @@ class TestGetMonitoringLocations(TestCase):
             return {'next': None, 'count': 2, 'results': ['dummyone', 'dummytwo']}
 
     def setUp(self):
+        logging.getLogger().setLevel(level=logging.INFO)
+        extract.FETCH_RETRY_DELAY = 0
         self.fake_endpoint = 'https://fake.usgs.gov/registry/monitoring-locations/'
+        self.mock_json_payload = {'good': 'json'}
+        self.mock_session = mocki(Session)
+        when(self.mock_session).__enter__().thenReturn(self.mock_session)
+        when(self.mock_session).__exit__(*mockito.args)
+
+        # to be called for testing get_monitoring_locations because it uses a with-block
+        # mockito.verifyStubbedInvocationsAreUsed()
 
     @mock.patch.object(Session, 'get', return_value=MockResponse(200))
     def test_get_data(self, _):
-        self.assertEqual(get_monitoring_locations(self.fake_endpoint), ['dummyone', 'dummytwo'])
+        self.assertEqual(extract.get_monitoring_locations(self.fake_endpoint), ['dummyone', 'dummytwo'])
+
+    @mock.patch('requests.session')
+    def testing_mocking_session(self, mock_session):
+        response1 = mock_session.get('some url')
+        response2 = mock_session.get('some url')
+        self.assertEqual(2, mock_session.get.call_count)
+        self.assertTrue(response1 is response2)
+
+    def testing_mockito_session_ab(self):
+        url_a = 'http://fake.mon-loc.net/a'
+        url_b = 'http://fake.mon-loc.net/b'
+        mock_response_a = mocki({'text': 'response test text A', 'status_code': 200}, spec=Response)
+        mock_response_b = mocki({'text': 'response test text B', 'status_code': 204}, spec=Response)
+        when(self.mock_session).get(url_a).thenReturn(mock_response_a)
+        when(self.mock_session).get(url_b).thenReturn(mock_response_b)
+
+        response1 = self.mock_session.get(url_a)
+        response2 = self.mock_session.get(url_a)
+        response3 = self.mock_session.get(url_b)
+
+        self.assertTrue(response1 is response2)
+        self.assertFalse(response1 is response3)
+
+        self.assertEqual(200, response1.status_code)
+        self.assertEqual(204, response3.status_code)
+
+    def testing_mockito_session_thenReturn(self):
+        mock_response_a = mocki({'text': 'response test text A', 'status_code': 200}, spec=Response)
+        mock_response_b = mocki({'text': 'response test text B', 'status_code': 204}, spec=Response)
+        when(self.mock_session).get(self.fake_endpoint).thenReturn(mock_response_a).thenReturn(mock_response_b)
+
+        response1 = self.mock_session.get(self.fake_endpoint)
+        response2 = self.mock_session.get(self.fake_endpoint)
+
+        self.assertFalse(response1 is response2)
+
+        self.assertEqual(200, response1.status_code)
+        self.assertEqual(204, response2.status_code)
+
+    def test_fetch_record_block_2_bad_json(self):
+        # this test ensures that the retries works and that if the JSON is bad that it only tries TWICE
+        self.assertEqual(extract.FETCH_TRIES_FOR_JSON, 2)  # testing that it is reset between tests
+
+        mock_response_a = mocki({'text': 'bad JSON twice', 'status_code': 200}, spec=Response)
+        when(self.mock_session).get(self.fake_endpoint).thenReturn(mock_response_a)
+        when(mock_response_a).json().thenRaise(JSONDecodeError('', '', 0))
+
+        self.assertRaises(JSONDecodeError, lambda: extract.fetch_record_block(self.fake_endpoint, self.mock_session))
+        mockito.verify(self.mock_session, times=extract.FETCH_TRIES_FOR_JSON).get(self.fake_endpoint)
+
+    def test_fetch_record_block_3_bad_json(self):
+        # this test ensures that the retries works and that if the JSON is bad that it tries THREE times
+        self.assertEqual(extract.FETCH_TRIES_FOR_JSON, 2)  # testing that it is reset between tests
+
+        mock_response_a = mocki({'text': 'bad JSON thrice', 'status_code': 200}, spec=Response)
+        when(self.mock_session).get(self.fake_endpoint).thenReturn(mock_response_a)
+        when(mock_response_a).json().thenRaise(JSONDecodeError('', '', 0))
+
+        extract.FETCH_TRIES_FOR_JSON = 3
+        self.assertRaises(JSONDecodeError, lambda: extract.fetch_record_block(self.fake_endpoint, self.mock_session))
+        mockito.verify(self.mock_session, times=extract.FETCH_TRIES_FOR_JSON).get(self.fake_endpoint)
+
+    def test_fetch_record_block_1_bad_json(self):
+        # this test ensures that the retries works and that if the JSON is bad the first time that it can succeed after
+        self.assertEqual(extract.FETCH_TRIES_FOR_JSON, 2)  # testing that it is reset between tests
+
+        mock_response_a = mocki({'text': 'bad JSON once', 'status_code': 200}, spec=Response)
+        when(self.mock_session).get(self.fake_endpoint).thenReturn(mock_response_a)
+        when(mock_response_a).json().thenRaise(JSONDecodeError('', '', 0)).thenReturn(self.mock_json_payload)
+
+        json_response = extract.fetch_record_block(self.fake_endpoint, self.mock_session)
+        mockito.verify(self.mock_session, times=2).get(self.fake_endpoint)
+
+        self.assertEqual(self.mock_json_payload, json_response)
+
+    def test_fetch_record_block_1_bad_status(self):
+        # this test ensures that the retries works and that if the STATUS CODE is bad the first time that it can succeed after
+        self.assertEqual(extract.FETCH_TRIES_FOR_STATUS_CODE, 2)  # testing that it is reset between tests
+
+        mock_response_a = mocki({'text': 'bad JSON once', 'status_code': 300}, spec=Response)
+        mock_response_b = mocki({'text': 'place holder', 'status_code': 200}, spec=Response)
+        when(self.mock_session).get(self.fake_endpoint).thenReturn(mock_response_a).thenReturn(mock_response_b)
+        when(mock_response_a).raise_for_status().thenRaise(HTTPError('http error msg', response=mock_response_a))
+        when(mock_response_b).raise_for_status().thenRaise(HTTPError("shouldn't call this", response=mock_response_b))
+        when(mock_response_a).json().thenRaise(JSONDecodeError("shouldn't call this", 'place holder', 0))
+        when(mock_response_b).json().thenReturn(self.mock_json_payload)
+
+        json_response = extract.fetch_record_block(self.fake_endpoint, self.mock_session)
+        mockito.verify(self.mock_session, times=2).get(self.fake_endpoint)
+
+        self.assertEqual(self.mock_json_payload, json_response)
+
+    def test_fetch_record_block_2_bad_status(self):
+        # this test ensures that the retries works and that if the STATUS CODE is bad that it only tries TWICE
+        self.assertEqual(extract.FETCH_TRIES_FOR_STATUS_CODE, 2)  # testing that it is reset between tests
+
+        mock_response_a = mocki({'text': 'bad status code twice', 'status_code': 300}, spec=Response)
+        when(mock_response_a).raise_for_status().thenRaise(HTTPError('http error msg', response=mock_response_a))
+        when(self.mock_session).get(self.fake_endpoint).thenReturn(mock_response_a)
+        when(mock_response_a).json().thenRaise(JSONDecodeError("shouldn't call this", 'place holder', 0))
+
+        # the HTTPError is captured by the tries and reported as a the parent RequestException indicating non-json error
+        self.assertRaises(RequestException, lambda: extract.fetch_record_block(self.fake_endpoint, self.mock_session))
+        mockito.verify(self.mock_session, times=extract.FETCH_TRIES_FOR_STATUS_CODE).get(self.fake_endpoint)
+
+    def test_fetch_record_block_3_bad_status(self):
+        # this test ensures that the retries works and that if the STATUS CODE is bad that it only tries THRICE
+        self.assertEqual(extract.FETCH_TRIES_FOR_STATUS_CODE, 2)  # testing that it is reset between tests
+
+        mock_response_a = mocki({'text': 'bad status code twice', 'status_code': 300}, spec=Response)
+        when(mock_response_a).raise_for_status().thenRaise(HTTPError('http error msg', response=mock_response_a))
+        when(self.mock_session).get(self.fake_endpoint).thenReturn(mock_response_a)
+        when(mock_response_a).json().thenRaise(JSONDecodeError("shouldn't call this", 'place holder', 0))
+
+        extract.FETCH_TRIES_FOR_STATUS_CODE = 3
+        # the HTTPError is captured by the tries and reported as a the parent RequestException indicating non-json error
+        self.assertRaises(RequestException, lambda: extract.fetch_record_block(self.fake_endpoint, self.mock_session))
+        mockito.verify(self.mock_session, times=extract.FETCH_TRIES_FOR_STATUS_CODE).get(self.fake_endpoint)
+
+    def test_fetch_record_block_1_bad_network(self):
+        # this test ensures that the retries works and that if the NETWORK is bad the first time and succeeds after
+        self.assertEqual(extract.FETCH_TRIES_FOR_NETWORK_ERROR, 2)  # testing that it is reset between tests
+
+        mock_response_a = mocki({'text': 'place holder', 'status_code': 200}, spec=Response)
+        when(self.mock_session).get(self.fake_endpoint)\
+            .thenRaise(RequestException(response=mock_response_a)).thenReturn(mock_response_a)
+        when(mock_response_a).json().thenReturn(self.mock_json_payload)
+
+        json_response = extract.fetch_record_block(self.fake_endpoint, self.mock_session)
+        mockito.verify(self.mock_session, times=2).get(self.fake_endpoint)
+
+        self.assertEqual(self.mock_json_payload, json_response)
+
+    def test_fetch_record_block_2_bad_network(self):
+        # this test ensures that the retries works and that if the NETWORK is bad that it only tries TWICE
+        self.assertEqual(extract.FETCH_TRIES_FOR_NETWORK_ERROR, 2)  # testing that it is reset between tests
+
+        mock_response_a = mocki({'text': 'bad JSON once', 'status_code': 200}, spec=Response)
+        when(self.mock_session).get(self.fake_endpoint).thenRaise(RequestException(response=mock_response_a))\
+
+        self.assertRaises(RequestException, lambda: extract.fetch_record_block(self.fake_endpoint, self.mock_session))
+        mockito.verify(self.mock_session, times=extract.FETCH_TRIES_FOR_NETWORK_ERROR).get(self.fake_endpoint)
+
+    def test_fetch_record_block_3_bad_network(self):
+        # this test ensures that the retries works and that if the NETWORK is bad that it only tries THRICE
+        self.assertEqual(extract.FETCH_TRIES_FOR_NETWORK_ERROR, 2)  # testing that it is reset between tests
+
+        mock_response_a = mocki({'text': 'bad JSON once', 'status_code': 200}, spec=Response)
+        when(self.mock_session).get(self.fake_endpoint).thenRaise(RequestException(response=mock_response_a))\
+
+        extract.FETCH_TRIES_FOR_NETWORK_ERROR = 3
+        self.assertRaises(RequestException, lambda: extract.fetch_record_block(self.fake_endpoint, self.mock_session))
+        mockito.verify(self.mock_session, times=extract.FETCH_TRIES_FOR_NETWORK_ERROR).get(self.fake_endpoint)
+
+    def test_fetch_record_block_1_none_response(self):
+        # this test ensures that the retries works and that if the response is None the first time and succeeds after
+        self.assertEqual(extract.FETCH_TRIES_FOR_NETWORK_ERROR, 2)  # testing that it is reset between tests
+
+        mock_response_a = mocki({'text': 'place holder', 'status_code': 200}, spec=Response)
+        when(self.mock_session).get(self.fake_endpoint).thenReturn(None).thenReturn(mock_response_a)
+        when(mock_response_a).json().thenReturn(self.mock_json_payload)
+
+        json_response = extract.fetch_record_block(self.fake_endpoint, self.mock_session)
+        mockito.verify(self.mock_session, times=2).get(self.fake_endpoint)
+
+        self.assertEqual(self.mock_json_payload, json_response)
+
+    def test_fetch_record_block_2_none_response(self):
+        # this test ensures that the retries works and that if the response is None the first time and succeeds after
+        self.assertEqual(extract.FETCH_TRIES_FOR_NETWORK_ERROR, 2)  # testing that it is reset between tests
+
+        when(self.mock_session).get(self.fake_endpoint).thenReturn(None)
+
+        self.assertRaises(RequestException, lambda: extract.fetch_record_block(self.fake_endpoint, self.mock_session))
+        mockito.verify(self.mock_session, times=extract.FETCH_TRIES_FOR_NETWORK_ERROR).get(self.fake_endpoint)
+
+    def test_fetch_record_block_3_none_response(self):
+        # this test ensures that the retries works and that if the response is None the first time and succeeds after
+        self.assertEqual(extract.FETCH_TRIES_FOR_NETWORK_ERROR, 2)  # testing that it is reset between tests
+
+        when(self.mock_session).get(self.fake_endpoint).thenReturn(None)
+
+        extract.FETCH_TRIES_FOR_NETWORK_ERROR = 3
+        try:
+            extract.fetch_record_block(self.fake_endpoint, self.mock_session)
+        except RequestException:
+            mockito.verify(self.mock_session, times=extract.FETCH_TRIES_FOR_NETWORK_ERROR).get(self.fake_endpoint)
+            return
+        self.fail('expected RequestException')
